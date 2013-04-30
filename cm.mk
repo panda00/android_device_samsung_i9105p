2bc0fa808313f8545ae850cd6351c3e619455916
skullface1 <bakreambarish4@gmail.com>
2013-04-30 18:38:49 +0530

    i9105-ization

--- cm.mk
+++ cm.mk
@@ -2,19 +2,19 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := GalaxyGrandDuos
+PRODUCT_RELEASE_NAME := GalaxyS2Plus
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/i9082/device_i9082.mk)
+$(call inherit-product, device/samsung/i9105/device_i9105.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := i9082
-PRODUCT_NAME := cm_i9082
+PRODUCT_DEVICE := i9105
+PRODUCT_NAME := cm_i9105
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9082
+PRODUCT_MODEL := GT-i9105
 
 #Set build fingerprint / ID / Prduct Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinvj TARGET_DEVICE=baffin BUILD_FINGERPRINT=samsung/baffinvj/baffin:4.1.2/JZO54K/I9082LDCAMC2:user/release-keys PRIVATE_BUILD_DESC="baffinvj-user 4.1.2 JZO54K I9082LDCAMC2 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=baffinvj TARGET_DEVICE=baffin BUILD_FINGERPRINT=samsung/baffinvj/baffin:4.1.2/JZO54K/i9105LDCAMC2:user/release-keys PRIVATE_BUILD_DESC="baffinvj-user 4.1.2 JZO54K i9105LDCAMC2 release-keys"
